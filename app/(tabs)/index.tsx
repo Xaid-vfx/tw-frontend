09e24b969adb90795bb36c48e39f8c9e6641bf9f
Xaid-vfx <mohd.zaid.123786@gmail.com>
2025-07-11 18:41:18 +0530

    fix top margin

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 20,
-    marginTop: 4,
+    marginTop: 8,
   },
   appTitle: {
     fontSize: 25,
